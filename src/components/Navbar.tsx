b83481a276bc3666fea6b7a62bf3da310d18926f
Vader <manavhirani@gmail.com>
2024-04-01 20:06:50 -0400

    Updated formatting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,14 +10,26 @@ export default function Navbar() {
     <nav className="flex w-full h-32 p-5 overflow-visible bg-gray-100">
       <div className="min-w-48 max-w-min">
         <Link className="flex items-center justify-center h-full" to="/" >
-          <img className="w-full" src={logo} alt="KigumiGroup"></img>
+          <img className="w-full" src={logo} alt="KigumiGroup" />
         </Link>
       </div>
       <span className="flex-grow"></span>
       <div className="flex items-center justify-between gap-10">
-        <Link id="trainings" to="/trainings" className="p-5 rounded-full hover:bg-green-200 active:bg-green-400"><p className="text-center">{t("Browse Trainings")}</p></Link>
-        <Link id="about" to="/about" className="p-5 rounded-full hover:bg-green-200"><p className="text-center">{t("About")}</p></Link>
-        <Link id="signin" to="/signin" className="p-5 rounded-full hover:bg-green-200"><p className="text-center">{t("Sign In")}</p></Link>
+        <Link id="trainings" to="/trainings" className="p-5 rounded-full hover:bg-green-200 active:bg-green-400">
+          <p className="text-center">
+            {t("Browse Trainings")}
+          </p>
+        </Link>
+        <Link id="about" to="/about" className="p-5 rounded-full hover:bg-green-200 active:bg-green-400">
+          <p className="text-center">
+            {t("About")}
+          </p>
+        </Link>
+        <Link id="signin" to="/signin" className="p-5 rounded-full hover:bg-green-200 active:bg-green-400">
+          <p className="text-center">
+            {t("Sign In")}
+          </p>
+        </Link>
         <LanguageSelector />
       </div>
     </nav>
